b8d5c5ebc24b97c14d709b9de1cf0c785e97fcb7
xjc <ccyxxjc@126.com>
2018-12-18 23:55:22 +0800

    完成图书信息页面

--- src/components/book_manager/book-info.js
+++ src/components/book_manager/book-info.js
@@ -10,6 +10,7 @@ export default class BookInfo extends Component{
             dataSource:[],
             visible:false,
             loading:false,
+            visible_c:false,
             price:''
         }
     }
@@ -32,18 +33,65 @@ export default class BookInfo extends Component{
     }
 
     handleChangeInfo = (e) => {
-        let id = e.target.dataset.id;
+        let id = parseInt(e.target.dataset.id);
+        this.setState({
+            loading:true,
+        });
+        //获取书籍信息
+        fetch('http://localhost:8080/book-detail',{
+                method:'post',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({book_id:id})
+            }).then((res) => {
+                console.log(res.status);
+                return res.json();
+            }).then((d) => {
+                // let d = JSON.parse(data);
+                this.setState({
+                    loading:false,
+                    visible_c:true,
+                    price:d.price
+                });
+                document.getElementById("book-name-c").value  = d.name;
+                document.getElementById("book-article-c").value = d.article;
+                document.getElementById("book-publisher-c").value = d.publisher;
+                document.getElementById("book-id-c").value = id;
+            }).catch((err) => {
+                console.log(err);
+            })
     }
 
     handleDeleteInfo = (e) => {
-        let id = e.target.dataset.id;
+        let id = parseInt(e.target.dataset.id);
+        fetch('http://localhost:8080/delete',{
+                method:'post',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify({book_id:id})
+            }).then((res) => {
+                console.log(res.status);
+                return res.json();
+            }).then((data) => {
+                if(data.code == 200){
+                    message.success('删除成功');
+                }else{
+                    message.error('删除失败');
+                }
+            }).catch((err) => {
+                console.log(err);
+            }).then(() => {
+                this.getBooks();
+            });
     }
 
     //[start] add-book
     handleAddBook = () => {
         this.setState({
             visible:true
-        })
+        });
     }
 
     handleAddBookOk = () => {
@@ -101,9 +149,72 @@ export default class BookInfo extends Component{
         }
     }
 
+    handleAddBookOk_c = () => {
+        this.setState({
+            loading:true
+        });
+        let id = document.getElementById("book-id-c").value;
+        let name = document.getElementById("book-name-c").value;
+        let price = document.getElementById("book-price-c").value;
+        let article = document.getElementById("book-article-c").value;
+        let publisher = document.getElementById("book-publisher-c").value;
+        if(!name){
+            message.error('请输入书名');
+        }else if(price == '￥ '){
+            message.error('请输入价格');
+        }else if(!article){
+            message.error('请输入作者');
+        }else if(!publisher){
+            message.error('请输入出版社');
+        }else{
+            let book = {
+                id,
+                name,
+                price:parseFloat(price.substring(2,price.length)),
+                article,
+                publisher,
+                pass:1
+            }
+            fetch('http://localhost:8080/change-book',{
+                method:'post',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body:JSON.stringify(book)
+            }).then((res) => {
+                console.log(res.status);
+                return res.json();
+            }).then((data) => {
+                if(data.code == 200){
+                    message.success('修改成功');
+                }else{
+                    message.error('修改失败');
+                }
+            }).catch((err) => {
+                console.log(err);
+            }).then(() => {
+                this.getBooks();
+            });
+            this.setState({
+                loading:false,
+                visible_c:false,
+                price:''
+            });
+            document.getElementById("book-name-c").value  = '';
+            document.getElementById("book-article-c").value = '';
+            document.getElementById("book-publisher-c").value = '';
+        }
+    }
+
     handleAddBookCancle = () => {
         this.setState({
             visible:false
+        })
+    }
+
+    handleAddBookCancle_c = () => {
+        this.setState({
+            visible_c:false
         })
     }
 
@@ -117,7 +228,7 @@ export default class BookInfo extends Component{
 
     render(){
         let _this = this;
-        const {visible,loading,price} = this.state;
+        const {visible,loading,price,visible_c} = this.state;
         const columns = [
             {
                 title:'ID',
@@ -173,7 +284,7 @@ export default class BookInfo extends Component{
                     onCancel={this.handleAddBookCancle}
                     footer={[
                         <Button key='back' type='default' onClick={this.handleAddBookCancle}>取消</Button>,
-                        <Button key='submit' type='primary' onClick={this.handleAddBookOk}>确定</Button>
+                        <Button key='submit' type='primary' onClick={this.handleAddBookOk} loading={loading}>确定</Button>
                     ]}
                 >
                     <Input id="book-name"
@@ -193,8 +304,38 @@ export default class BookInfo extends Component{
                         placeholder='请输入出版社'
                     ></Input>
                 </Modal>
+                <Modal
+                    visible={visible_c}
+                    title='修改图书'
+                    onOk={this.handleAddBookOk_c}
+                    onCancel={this.handleAddBookCancle_c}
+                    footer={[
+                        <Button key='back' type='default' onClick={this.handleAddBookCancle_c}>取消</Button>,
+                        <Button key='submit' type='primary' onClick={this.handleAddBookOk_c} loading={loading}>确定</Button>
+                    ]}
+                >
+                    <Input id="book-name-c"
+                        placeholder='请输入书名'
+                    ></Input>
+                    <InputNumber id="book-price-c"
+                        placeholder='请输入价格'
+                        value={price}
+                        onChange={value => {this.changePrice(value)}}
+                        formatter={value => `￥ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                        parser={value => value.replace(/￥\s?|(,*)/g, '')}
+                    ></InputNumber>
+                    <Input id="book-article-c"
+                        placeholder='请输入作者'
+                    ></Input>
+                    <Input id="book-publisher-c"
+                        placeholder='请输入出版社'
+                    ></Input>
+                    <Input id="book-id-c"
+                        style={{display:'none'}}
+                    ></Input>
+                </Modal>
                 <div id='add-book-bar'>
-                    <Button type='primary' id='add-book-btn' loading={loading} onClick={this.handleAddBook}>添加图书</Button>
+                    <Button type='primary' id='add-book-btn' onClick={this.handleAddBook}>添加图书</Button>
                 </div>
                 <Table columns={columns} dataSource={this.state.dataSource}></Table>
             </div>
